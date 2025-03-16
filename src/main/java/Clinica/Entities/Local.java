414c97b8495f5b0f8236fbba09148621f2277499
Ruan <ruancd972@gmail.com>
2025-03-16 16:25:00 -0300

    resolvendo um pequeno erro

--- src/main/java/Clinica/Entities/Local.java
+++ src/main/java/Clinica/Entities/Local.java
@@ -8,6 +8,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.Lob;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
@@ -53,7 +54,7 @@ public class Local {
 	}
 	
 	
-	@OneToMany
+	@OneToMany(mappedBy = "local")
 	List<Agendamento> agendamento;
 	
 
